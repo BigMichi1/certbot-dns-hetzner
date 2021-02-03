a512c8d70b398cf9337e4fe2706619b56e9543cb
Michael Cramer <michael@bigmichi1.de>
2021-02-03 08:18:55 +0100

    regnerate build files with certbot 1.12.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ version = '1.0.5'
 # specified here to avoid masking the more specific request requirements in
 # acme. See https://github.com/pypa/pip/issues/988 for more info.
 install_requires = [
-    'setuptools',
+    'setuptools>=39.0.1',
     'zope.interface',
     'requests',
     'mock',
@@ -54,7 +54,7 @@ setup(
     author="ctrl.alt.coop",
     author_email='kontakt@ctrl.alt.coop',
     license='Apache License 2.0',
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*',
+    python_requires='>=3.6',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Plugins',
@@ -62,12 +62,11 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Security',
         'Topic :: System :: Installation/Setup',
